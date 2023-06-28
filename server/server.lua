c8b07731a89d0c07a11218090f1f6bfe451aa572
StunZ <115426921+realstunz@users.noreply.github.com>
2023-06-28 15:29:03 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,9 +3,13 @@ local QBCore = exports['qb-core']:GetCoreObject()
 local mixerInUse = false -- DO NOT CHANGE THIS
 
 RegisterNetEvent('sz-pizzajob:server:makedough', function(data)
-    mixerInUse = true -- No one can use the mixer
     local src = source
-    QBCore.Functions.Notify(src, 'Began mixing...', 'primary', 3000)
-    Wait(Config.MixerWaitTime * 1000)
-    QBCore.Functions.Notify(src, 'Mixer has finished', 'success', 3000)
+    if mixerInUse then
+        QBCore.Functions.Notify(src, 'Mixer already in use', 'error', 3000)
+    else
+        mixerInUse = true 
+        QBCore.Functions.Notify(src, 'Began mixing...', 'primary', 3000)
+        Wait(Config.MixerWaitTime * 1000)
+        QBCore.Functions.Notify(src, 'Mixer has finished', 'success', 3000)
+    end
 end)
